3e8d59bb3d65b5b5a67c78ae634b234e3d8c3307
mdos-san <mdossan@protonmail.com>
2024-11-06 23:50:37 +0100

    test: fix test with new uppercase http method

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,9 @@
 import { HttpRequest } from "./index.js"
 
 test("`HttpRequest` can be created", () => {
-  const request = new HttpRequest("post", "/cats", { "Content-Type": "application/json" }, { name: "Scratchy", food: "chicken" });
+  const request = new HttpRequest("POST", "/cats", { "Content-Type": "application/json" }, { name: "Scratchy", food: "chicken" });
   expect(request).toBeDefined();
-  expect(request.method).toBe("post")
+  expect(request.method).toBe("POST")
   expect(request.path).toBe("/cats")
   expect(request.headers).toStrictEqual({ "Content-Type": "application/json" })
   expect(request.body).toStrictEqual({ name: "Scratchy", food: "chicken" })
